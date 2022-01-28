d92b1920e01d8001bf08f3e3d3a72d3961f6d5fd
Shiv10 <shivanshsharma2012@gmail.com>
2022-01-28 22:27:17 +0530

    [WIP]: store data route

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import express from 'express'
 import singupRouter from './routes/signup';
 import authorize from './middlewares/auth';
 import loginRouter from './routes/login';
+import productRouter from './routes/storeItem';
 
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/signup', singupRouter);
 app.use('/login', loginRouter);
+app.use('/storeProduct', authorize, productRouter);
 app.get('/', authorize, async (req: Request, res: Response)=> {
     res.send("testing")
 })
---
+++ src/routes/storeItem.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { Request, Response } from "express";
+import products from "../models/products";
+
+const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+
+});
+
+export default router;
