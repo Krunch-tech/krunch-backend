218a63e6861f98f31e89a7317530413bb521c313
Shiv10 <shivanshsharma2012@gmail.com>
2022-01-31 01:08:19 +0530

    feat: store custom data

--- src/routes/storeItem.ts
+++ src/routes/storeItem.ts
@@ -13,6 +13,36 @@ router.post('/', async (req: Request, res: Response) => {
         time = time.toString();
         const ip = req.ip;
         const location = lookup(ip);
+        const p = await products.find({ $and: [{ email: req.body.userInfo.email }, { name: name }] });
+        
+        if (p) {
+            res.status(200).json({success: false, error: 'Item with name already exists.'});
+            return;
+        }
+
+        let product = new products({
+            userEmail: req.body.userInfo.email,
+            productType: 'custom',
+            name: name,
+            picture: picture,
+            data: data,
+            time: time,
+            location: location,
+            like: like,
+            category: category,
+            tags: tags
+        });
+
+        try {
+            product = await product.save();
+            const id = product._id;
+            res.status(200).json({success: true, id: id});
+            return;
+        } catch(e) {
+            console.log(`Unable to save.\n${e}`);
+            res.status(200).json({success: false, error: "Database error"});
+            return;
+        }
     }
 });
 
