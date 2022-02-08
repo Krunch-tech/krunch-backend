4da3689ce842bb48b6a85da17cb45eea79bf602f
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-08 23:32:26 +0530

    feat: get all items

--- src/routes/getItems.ts
+++ src/routes/getItems.ts
@@ -6,7 +6,20 @@ const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
 
-    
+    try {
+        const product = await products.find({email: req.body.userInfo.email});
+
+        if(product.length!==0) {
+            res.status(200).json({success: true, products: product});
+            return;
+        } else {
+            res.status(200).json({success: false, error: 'No stored items'});
+            return;
+        }
+    } catch(e) {
+        res.status(200).json({success: false, error: 'Database connectivity issue.'});
+        return;
+    }
 
 });
 
