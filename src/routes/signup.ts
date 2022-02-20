aee3f3503261ed28c3c04e91c4917d6b49d41fd9
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-20 14:35:09 +0530

    feat: validated signup body

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -3,10 +3,29 @@ import { Request, Response} from 'express';
 import users from '../models/user';
 import hashPassword from '../tools/hash';
 import jwt from 'jsonwebtoken'
+import * as Joi from 'joi';
+import {
+    ContainerTypes,
+    ValidatedRequest,
+    ValidatedRequestSchema,
+    createValidator
+  } from 'express-joi-validation'
+import { valid } from 'joi';
 
 const router = express.Router();
 
-router.post('/', async (req: Request, res: Response) => {
+const validator = createValidator()
+
+const querySchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+  name: Joi.string().required(),
+  picture: Joi.string()
+});
+
+
+router.post('/',validator.body(querySchema), async (req: Request, res: Response) => {
+
 
     if (!(typeof req.body.email === 'string' && typeof req.body.password === 'string')) {
         res.status(400);
@@ -14,7 +33,7 @@ router.post('/', async (req: Request, res: Response) => {
         return;
     }
 
-    if (req.body.email.length > 150 || req.body.password.length > 150 || req.body.name.length > 150) {
+    if (req.body.email.length > 150 || req.body.password.length > 150 ) {
         res.status(400);
         res.json({success: false, error: 'Spam detected'});
         return;
@@ -32,7 +51,8 @@ router.post('/', async (req: Request, res: Response) => {
         email: req.body.email,
         password: await hashPassword(req.body.password),
         name: req.body.name,
-        authType: 'custom'
+        authType: 'custom',
+        picture: req.body.picture
     });
 
     try {
